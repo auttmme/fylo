83ae9e429dba7293976a8f9f5f4ff02a6aff8a9c
auttmme <ichautm14@gmail.com>
2022-06-14 17:43:28 +0700

    add new color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ module.exports = {
         'main': 'hsl(218, 28%, 13%)',
         'cyan': 'hsl(176, 68%, 64%)',
         'n-blue': 'hsl(198, 60%, 50%)',
-        'testi': 'hsl(219, 30%, 18%)'
+        'testi': 'hsl(219, 30%, 18%)',
+        'intro-blue': 'hsl(217, 28%, 15%)',
+        'footer': 'hsl(216, 53%, 9%)'
       }
     },
   },
