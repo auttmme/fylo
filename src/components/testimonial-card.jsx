fcf3199ebf47a446325c79483453dc596cd1ec02
auttmme <ichautm14@gmail.com>
2022-06-14 17:43:57 +0700

    refactor testimonial card

--- src/components/testimonial-card.jsx
+++ src/components/testimonial-card.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
 export default function TestimonialCard(props) {
+  const testimonial = props.testimonial
+  
 	return (
-			<div className='bg-testi text-white w-1/4 rounded-md p-8 font-open-sans'>
-				<p>{props.desc}</p>
-				<div className='flex items-center mt-4'>
-					<img className='rounded-full w-11 h-11 mr-3' src={props.image} alt="profile image" />
-					<div>
-						<p className='font-bold'>{props.name}</p>
-						<p>{props.role}</p>
-					</div>
-				</div>
-			</div>
+    <div className='grid grid-cols-3 gap-8 shadow-md'>
+      {testimonial.map((testi) => (
+        <div className='bg-testi text-white rounded-sm p-8 font-open-sans'>
+          <p className='font-open-sans'>{testi.desc}</p>
+          <div className='flex items-center mt-4'>
+            <img className='rounded-full w-11 h-11 mr-3' src={testi.image} alt="profile image" />
+            <div>
+              <p className='font-bold'>{testi.name}</p>
+              <p className='font-open-sans'>{testi.role}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
 	)
 }
