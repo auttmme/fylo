2d30665765f48c5cb24b55a8bcada84a17f0ca27
auttmme <ichautm14@gmail.com>
2022-06-15 17:35:15 +0700

    make component relative and adjust rounded corner

--- src/components/testimonial-card.jsx
+++ src/components/testimonial-card.jsx
@@ -4,9 +4,9 @@ export default function TestimonialCard(props) {
   const testimonial = props.testimonial
   
 	return (
-    <div className='grid grid-cols-3 gap-8 shadow-md'>
+    <div className='grid grid-cols-3 gap-8 shadow-md z-20 relative'>
       {testimonial.map((testi) => (
-        <div className='bg-testi text-white rounded-sm p-8 font-open-sans'>
+        <div className='bg-testi text-white rounded-lg p-8 font-open-sans'>
           <p className='font-open-sans'>{testi.desc}</p>
           <div className='flex items-center mt-4'>
             <img className='rounded-full w-11 h-11 mr-3' src={testi.image} alt="profile image" />
