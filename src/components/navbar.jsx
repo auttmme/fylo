3e3717288982021a114aa6358b36dc92d96ac645
auttmme <ichautm14@gmail.com>
2022-06-16 23:32:53 +0700

    change background color

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.svg'
 
 export default function Navbar() {
   return (
-    <nav className='flex justify-between font-open-sans items-center py-10'>
+    <nav className='bg-intro-blue flex justify-between font-open-sans items-center py-10'>
       <img src={logo} alt="Logo" className='ml-12' />
       <div className='flex justify-evenly w-1/5 items-center'>
         <a className='text-white hover:underline hover:font-bold hover:cursor-pointer'>Features</a>
